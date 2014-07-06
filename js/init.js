57b19ab969b5d27318faac378462858e97ebc2ad
Bruno Sampaio <Bruno@c-3po.home>
2014-07-06 01:30:15 +0100

    v0.3

--- js/init.js
+++ js/init.js
@@ -2,7 +2,8 @@ var wi = {
 	
 	current : {
 		active : false,
-		dataBox : $()
+		dataBox : $(),
+		element : $()
 	},
 
 	css : {
@@ -40,13 +41,20 @@ var wi = {
 	},
 
 	init : function() {
-		var selector = this.elements.REFERENCE.join(',') + ',' + this.elements.TEXT.join(',') + ',' + this.elements.MULTIMEDIA.join(',');
-		var containers = this.elements.CONTENT.join(',');
+		var selector = wi.elements.REFERENCE.join(',') + ',' + wi.elements.TEXT.join(',') + ',' + wi.elements.MULTIMEDIA.join(',');
+		var containers = wi.elements.CONTENT.join(',');
 
 		// Remove Data Box when user clicks outside
 		$(document).on('click', function(event) {
 			if(wi.current.active) {
 				wi.removeDataBox();
+			}
+		});
+
+		// Move Data Box on scroll
+		$(window).on('scroll', function(event) {
+			if(wi.current.active && wi.current.dataBox.length > 0 && wi.current.element.length > 0) {
+				wi.calculateDataBoxPosition(wi.current.dataBox, wi.current.element);
 			}
 		});
 
@@ -69,18 +77,18 @@ var wi = {
 	},
 
 	addDataBox : function(domToInspect) {
-		var contentBox = $('<div class="' + this.css.CONTENT_BOX_CLASS + '"></div>');
-		var dataBox = $('<div class="' + this.css.DATA_BOX_CLASS + '"></div>');
+		var contentBox = $('<div class="' + wi.css.CONTENT_BOX_CLASS + '"></div>');
+		var dataBox = $('<div class="' + wi.css.DATA_BOX_CLASS + '"></div>');
 
 		var tagName = domToInspect[0].tagName;
-		var position = domToInspect.offset();
 		var metadata = [];
+
+		// Set Content Box Position
+		wi.calculateDataBoxPosition(contentBox, domToInspect);
 
 		// Set Content Box Styles
 		contentBox.css({
-			width : (domToInspect.outerWidth() + 20) + 'px',
-			top: position.top - 10,
-			left: position.left - 10
+			width : (domToInspect.outerWidth() + 20) + 'px'
 		});
 
 		// Set Data Box Styles
@@ -89,14 +97,14 @@ var wi = {
 		});
 
 		// Set Data Box Content
-		if(this.elements.MULTIMEDIA.indexOf(tagName) != -1) {
-			metadata = this.getMultimediaMetadata(domToInspect);
+		if(wi.elements.MULTIMEDIA.indexOf(tagName) != -1) {
+			metadata = wi.getMultimediaMetadata(domToInspect);
 		}
-		else if(this.elements.REFERENCE.indexOf(tagName) != -1) {
-			metadata = this.getReferenceMetadata(domToInspect);
+		else if(wi.elements.REFERENCE.indexOf(tagName) != -1) {
+			metadata = wi.getReferenceMetadata(domToInspect);
 		}
-		else if(this.elements.TEXT.indexOf(tagName) != -1) {
-			metadata = this.getTextMetadata(domToInspect);
+		else if(wi.elements.TEXT.indexOf(tagName) != -1) {
+			metadata = wi.getTextMetadata(domToInspect);
 		}
 
 		var htmlMetadata = '<form>';
@@ -104,7 +112,7 @@ var wi = {
 			var field = metadata[i];
 			if(field.value) {
 				htmlMetadata+= 
-					'<div class="' + this.css.DATA_FIELD + '">' +
+					'<div class="' + wi.css.DATA_FIELD + '">' +
 						'<label for="' + field.id + '">' + field.label + ':</label>' +
 						(field.id.indexOf('src') != -1?
 							'<a href="' + field.value + '" target="_blank">' + field.value + '</a>' :
@@ -127,11 +135,21 @@ var wi = {
 		// Add Content Box to Document
 		$('body').append(contentBox);
 
-		this.current.dataBox = contentBox;
+		wi.current.dataBox = contentBox;
+		wi.current.element = domToInspect;
+	},
+
+	calculateDataBoxPosition : function(contentBox, domToInspect) {
+		var position = domToInspect.offset();
+
+		contentBox.css({
+			top: position.top - 10,
+			left: position.left - 10
+		});
 	},
 
 	getMultimediaMetadata : function(domToInspect) {
-		var source = this.utils.calculateSourceURL(domToInspect[0].getAttribute('src') || domToInspect.children('source')[0].getAttribute('src'));
+		var source = wi.utils.parseSourceURL(domToInspect[0].getAttribute('src') || domToInspect.children('source')[0].getAttribute('src'));
 
 		return [
 			{ id : 'width', label : chrome.i18n.getMessage("fieldWidthLabel"), value : domToInspect.width() + 'px' },
@@ -142,49 +160,63 @@ var wi = {
 	},
 
 	getReferenceMetadata : function(domToInspect) {
-		var source = this.utils.calculateSourceURL(domToInspect[0].getAttribute('href') || domToInspect[0].getAttribute('src'));
+		var source = wi.utils.parseSourceURL(domToInspect[0].getAttribute('href') || domToInspect[0].getAttribute('src'));
 
-		return this.getTextMetadata(domToInspect).concat([
+		return wi.getTextMetadata(domToInspect).concat([
 			{ id : 'src', label : chrome.i18n.getMessage("fieldSourceLabel"), value : source },
 			{ id : 'lang', label : chrome.i18n.getMessage("fieldLangLabel"), value : domToInspect.attr('hreflang') }
 		])
 	},
 
 	getTextMetadata : function(domToInspect) {
-		var backgroundURL = this.utils.calculateSourceURL(domToInspect.css('background-image').replace('url(','').replace(')',''));
+		var textColor = wi.utils.parseColor(domToInspect.css('color'));
+		var backgroundColor = wi.utils.parseColor(domToInspect.css('background-color'));
+		var backgroundImage = wi.utils.parseBackgroundImage(domToInspect.css('background-image'));
 
 		return [
 			{ id : 'font', label : chrome.i18n.getMessage("fieldFontLabel"), value : domToInspect.css('font-family') },
 			{ id : 'fsize', label : chrome.i18n.getMessage("fieldFSizeLabel"), value : domToInspect.css('font-size') },
-			{ id : 'text-color', label : chrome.i18n.getMessage("fieldTextColorLabel"), value : domToInspect.css('color') },
-			{ id : 'image-src', label : chrome.i18n.getMessage("fieldBackgroundImageLabel"), value : backgroundURL }
+			{ id : 'text-color', label : chrome.i18n.getMessage("fieldTextColorLabel"), value : textColor },
+			{ id : 'background-color', label : chrome.i18n.getMessage("fieldBackgroundColorLabel"), value : backgroundColor },
+			{ id : 'image-src', label : chrome.i18n.getMessage("fieldBackgroundImageLabel"), value : backgroundImage }
 		]
 	},
 
 	removeDataBox : function() {
-		this.current.dataBox.remove();
+		wi.current.dataBox.remove();
+		wi.current.element = $();
 	},
 
 	toggleStatus : function() {
-		this.current.active = !this.current.active;
+		wi.current.active = !wi.current.active;
 
-		if(this.current.active) {
-			$('body').addClass(this.css.ACTIVE);
+		if(wi.current.active) {
+			$('body').addClass(wi.css.ACTIVE);
 		}
 		else {
-			$('body').removeClass(this.css.ACTIVE);
-			this.removeDataBox();
+			$('body').removeClass(wi.css.ACTIVE);
+			wi.removeDataBox();
 		}
 	},
 
 	utils : {
-		calculateSourceURL : function(url) {
-			if(url && url.indexOf('://') == -1) {
-				url = window.location.protocol + '//' + window.location.host + (window.location.port? ':' + window.location.port : '' ) + '/' + url;
+
+		parseColor : function(str) {
+			return str == 'transparent'? '' : str;
+		},
+
+		parseBackgroundImage : function(str) {
+			return str == 'none'? '' : this.parseSourceURL(str.replace('url(','').replace(')',''))
+		},
+
+		parseSourceURL : function(str) {
+			if(str && str.indexOf('://') == -1) {
+				str = window.location.protocol + '//' + window.location.host + (window.location.port? ':' + window.location.port : '' ) + '/' + str;
 			}
 
-			return url;
+			return str;
 		}
+
 	}
 
 };
